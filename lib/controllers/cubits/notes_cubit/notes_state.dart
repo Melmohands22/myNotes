388593e060e207d38ca949323bfe4f97ec2da7d7
Melmohands22 <melmohands591@gmail.com>
2024-10-27 12:58:43 +0300

    add notes cubit

---
+++ lib/controllers/cubits/notes_cubit/notes_state.dart
@@ -0,0 +1,15 @@
+part of 'notes_cubit.dart';
+
+@immutable
+sealed class NotesState {}
+
+final class NotesInitial extends NotesState {}
+
+final class NotesSuccses extends NotesState {}
+
+final class NotesFailure extends NotesState {
+  final String errMessage;
+  NotesFailure(this.errMessage);
+}
+
+final class NotesLoading extends NotesState {}
